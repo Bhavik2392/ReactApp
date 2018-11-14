b3380c2818c9456140a874a079774ee1503eca4a
Bhavik2392 <45010893+Bhavik2392@users.noreply.github.com>
2018-11-14 14:04:47 -0500

    Added Fetch Data Api Project file
    Co-Authored-By: bhavik2392 <bhavik2392@users.noreply.github.com>

---
+++ fetch-data-api/fetch-data-api-build/precache-manifest.ea31a5cdb8ec986573f864970390df68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b7cc3a431bb1f92e6557daca088f7fb",
+    "url": "/static/media/bg.7b7cc3a4.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ebcaa853879b6b98976c",
+    "url": "/static/js/main.ebcaa853.chunk.js"
+  },
+  {
+    "revision": "927272c8bb0fb1ab1070",
+    "url": "/static/js/1.927272c8.chunk.js"
+  },
+  {
+    "revision": "ebcaa853879b6b98976c",
+    "url": "/static/css/main.82509a1b.chunk.css"
+  },
+  {
+    "revision": "927272c8bb0fb1ab1070",
+    "url": "/static/css/1.5ec28ade.chunk.css"
+  },
+  {
+    "revision": "c54181f9503157f56be16dcdbcb058e1",
+    "url": "/index.html"
+  }
+];
